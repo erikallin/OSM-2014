bb55bcbb8a6a832ec8cbdd8f44e1df33ba740986
erikallin <erik.allin@gmail.com>
2014-02-07 14:38:30 +0100

    Copy pasted c-code from assignment

--- ex1/task1/dlList.c
+++ ex1/task1/dlList.c
@@ -5,3 +5,21 @@
  {
   printf("\n\n\n\n\nVelkommen til OSM!\nNow Buenos make!\n\n\n\n\n");
  }
+ 
+ typedef int bool;
+  typedef void item; /* We store pointers, cuz why not? */
+
+  typedef struct node_ {
+    item           *thing;
+    struct node_   *ptr;
+  } node;
+  typedef struct dlist_ {
+    node *head, *tail;
+  } dlist;
+  
+  
+  void insert(dlist *this, item* thing, bool atTail);
+  item* extract(dlist *this, bool atTail);
+  void reserve(dlist *this);
+  
+  item* search(dlist *this, bool (*matches(item*)));
