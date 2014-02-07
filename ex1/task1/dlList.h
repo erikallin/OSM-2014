87d3f0af126f013fc1a3bde2610cd409dbf43c27
erikallin <erik.allin@gmail.com>
2014-02-07 15:03:05 +0100

    Moved to header file

---
+++ ex1/task1/dlList.h
@@ -0,0 +1,19 @@
+
+
+typedef int bool;
+typedef void item; /* We store pointers, cuz why not? */
+
+typedef struct node_ {
+  item           *thing;
+  struct node_   *ptr;
+} node;
+typedef struct dlist_ {
+  node *head, *tail;
+} dlist;
+
+
+void insert(dlist *this, item* thing, bool atTail);
+item* extract(dlist *this, bool atTail);
+void reserve(dlist *this);
+
+item* search(dlist *this, bool (*matches(item*)));
