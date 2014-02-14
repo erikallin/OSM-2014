8c16d5f1062adb44237783e80026dfc1726bc4e3
DennisOlesen <dennisolesen93@gmail.com>
2014-02-14 13:53:17 +0100

    readwrite virker nu og testes m med fyams-sim buenos 'initprog=[disk1]readwrite'

--- ex1/task2/tests/readwrite.c
+++ ex1/task2/tests/readwrite.c
@@ -1,12 +1,8 @@
 #include "lib.h"
-
 int main(void)
 {
- int i;
  int a[100];
- for(i = 0; i < 100; i++) {
-    a[i] = 'a';
- }
+    syscall_read(0,a,100); 
     syscall_write(1,a,100);
  return 0;
 }
